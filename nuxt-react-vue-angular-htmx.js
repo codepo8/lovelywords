7df81784d98689d6773a4c9dd976e927f11087d9
Christian Heilmann <chris.heilmann@gmail.com>
2025-03-29 08:34:22 +0000

    adding all the code

---
+++ nuxt-react-vue-angular-htmx.js
@@ -0,0 +1,30 @@
+let current = false;
+let dts = document.querySelectorAll('dt');
+let all = dts.length - 1;
+let show = num => {
+    if (current) {
+        current.className = ''
+    }
+    if (dts[num]) {
+        current = dts[num];
+        current.className = 'current';
+        window.location.hash = num + '-' + current.innerText;
+    } else {
+        let rand = Math.round(Math.random() * all);
+        current = dts[rand];
+        current.className = 'current';
+        window.location.hash = rand + '-' + current.innerText;
+    }
+}
+if (window.location.hash) {
+    let num = window.location.hash.split('-')[0].replace('#','');
+    if (dts[num]) {
+        show(num)
+    }
+} else {
+    show();
+}
+document.querySelector('button').addEventListener('click', ev => {
+    show();
+    ev.preventDefault();
+})
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Lovely words
+
+Just a collection of lovely words we should be using more.
+
+[Brighten up your vocabulary and your days](https://codepo8.github.io/lovelywords)
